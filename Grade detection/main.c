a49d9b496ba8105d0d427d0846d30daef8e2ef1a
ZyadYhia <zeyad.yhia95@gmail.com>
2020-08-08 11:10:02 +0200

    second task in 3rd session to test if condition

--- Grade detection/main.c
+++ Grade detection/main.c
@@ -4,13 +4,13 @@ int main()
 {
     int grade=0;
 
-    printf("Please Insert Your Grade..\n");
+    printf("Please Insert Your Grade..  ");
     scanf("%d",&grade);
 
-    if (grade>=85){printf("Your grade is Excellent");}
-    else if(grade>=75){printf("Your grade is Very Good");}
-    else if(grade>=65){printf("Your grade is Good");}
-    else if(grade>=50){printf("Your grade is Pass");}
-    else if(grade<50){printf("You Failed!!");}
+    if (grade>=85){printf("\nYour grade is Excellent\n");}
+    else if(grade>=75){printf("\nYour grade is Very Good\n");}
+    else if(grade>=65){printf("\nYour grade is Good\n");}
+    else if(grade>=50){printf("\nYour grade is Pass\n");}
+    else if(grade<50){printf("\nYou Failed!!\n");}
     return 0;
 }
